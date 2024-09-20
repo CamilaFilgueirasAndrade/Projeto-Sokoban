7f525638346e41e91cc4111000a37cf9efc66169
Camila_Filgueiras <camilafilgueiras.marketing@gmail.com>
2024-09-20 16:58:07 -0300

    Transformando função nexteposition e movePlayer em metodo.

--- jogo.js
+++ jogo.js
@@ -1,17 +1,39 @@
-window.addEventListener("keydown", function (event) {
-    const next = nextPosition(event.code);
-
-    if (verifyPosition(next)) movePlayer(next);
-})
-
 const player = new Player(0, 0);
 const celulas = document.querySelectorAll('.cell');
 const playerElement = document.querySelector('.player');
 
+window.addEventListener("keydown", function (event) {
+    const next = player.nextPosition(event.code);
+
+    if (verifyPosition(next)) player.movePlayer(next);
+})
 
 function Player(x, y) {
     this.x = x;
     this.y = y;
+
+    this.nextPosition = function(keycode) {
+        let { x, y } = player;
+    
+        if (keycode === "ArrowUp") x--;
+        if (keycode === "ArrowDown") x++;
+        if (keycode === "ArrowLeft") y--;
+        if (keycode === "ArrowRight") y++;
+        
+        return { x, y };
+    
+    }
+
+    this.movePlayer = function(position) {
+        let { x, y } = position;
+        this.x = x;
+        this.y = y;
+    
+        const k = x * 4 + y;
+    
+        celulas[k].append(playerElement);
+        console.log(position);
+    }
 }
 
 function nextPosition(keycode) {
@@ -25,17 +47,6 @@ function nextPosition(keycode) {
     console.log(keycode, player);
     return { x, y };
 
-}
-
-function movePlayer(position) {
-    let { x, y } = position;
-    player.x = x;
-    player.y = y;
-
-    const k = x * 4 + y;
-
-    celulas[k].append(playerElement);
-    console.log(position);
 }
 
 function verifyPosition(position) {
