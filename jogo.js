2f94306021dfe2de800fb9d86a0023caa51dfb91
Camila_Filgueiras <camilafilgueiras.marketing@gmail.com>
2024-09-25 17:20:59 -0300

    adicinando mapa atraves de caracteres

--- jogo.js
+++ jogo.js
@@ -1,7 +1,19 @@
+const boardMap = [
+    ["#", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", ".", ".", ".", ".", ".", ".", "#"],
+    ["#", ".", ".", ".", "#", ".", ".", "#"],
+    ["#", ".", "#", "G", ".", ".", ".", "#"],
+    ["#", ".", ".", "G", "B", "#", ".", "#"],
+    ["#", ".", ".", "#", ".", "B", ".", "#"],
+    ["#", ".", ".", "P", ".", ".", ".", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "#"]
+]
+
+const NUM_ROWS = boardMap.length;
+const NUM_COLS = boardMap[0].length;
+
 const DIST_SALTO = 66;
 const MARGIN_FIX = 4;
-const NUM_ROWS = 8;
-const NUM_COLS = 8;
 
 buildGameBoard(NUM_ROWS, NUM_COLS);
 
@@ -72,11 +84,18 @@ function buildGameBoard(numRows, numCols) {
     const game = document.getElementById("jogo");
     const board = createGameElement('div', 'tabuleiro', game);
     
-    for (let k = 0; k < numRows; k++) {
+    for (let i = 0; i < numRows; i++) {
         const row = createGameElement('div', 'row', board);
-        
-        for (let i = 0; i < numCols; i++) {
-            createGameElement('div', 'cell', row);
+
+        for (let j = 0; j < numCols; j++) {
+            const cell = createGameElement('div', 'cell', row);
+            const char = boardMap[i][j]
+            console.log(char);
+            
+            if(char === '#')cell.classList.add('wall')
+            if(char === 'G')cell.classList.add('goal')
+            if(char === 'B')cell.classList.add('box')
+    
         }
     }
     
