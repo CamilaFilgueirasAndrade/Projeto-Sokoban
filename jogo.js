bf8940e0cef5b27669f7b9e42032c633ea155b4b
Camila_Filgueiras <camilafilgueiras.marketing@gmail.com>
2024-09-27 15:55:19 -0300

    Cria função "creatBoardPiece"

--- jogo.js
+++ jogo.js
@@ -6,11 +6,17 @@ const MARGIN_FIX = 4;
 const pieces = buildGameBoard(NUM_ROWS, NUM_COLS);
 const board = document.querySelector('.tabuleiro');
 
-const playerElement = createGameElement('div', 'player', board);
 const player = new Piece(pieces.player.x, pieces.player.y);
+const playerElement = creatBoardPiece(player, 'player');
 
-playerElement.style.top = calculaPosicao(player.x);
-playerElement.style.left = calculaPosicao(player.y);
+function creatBoardPiece(piece, className){
+    const elemento = createGameElement('div', className, board);
+    
+    elemento.style.top = calculaPosicao(piece.x);
+    elemento.style.left = calculaPosicao(piece.y);
+    
+    return elemento;
+}
 
 
 window.addEventListener("keydown", function (event) {
@@ -22,8 +28,14 @@ window.addEventListener("keydown", function (event) {
     }
 })
 
+function verifyPosition(position) {
+    console.log(position);
+    let { x, y } = position;
 
+    return boardMap[x][y] !== '#';
+}
 
-
-
+function calculaPosicao(qtd) {
+    return `${qtd * DIST_SALTO + MARGIN_FIX}px`;
+}
 
